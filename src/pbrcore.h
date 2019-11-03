30abc1c8784ecf0bd3e8a511ab8b703f0852ac69
Victor Fisac <victorfisac@gmail.com>
2019-11-03 17:06:54 +0100

    Updated readme

--- src/pbrcore.h
+++ src/pbrcore.h
@@ -26,7 +26,7 @@
 *   rPBR is licensed under an unmodified zlib/libpng license, which is an OSI-certified,
 *   BSD-like license that allows static linking with closed source software:
 *
-*   Copyright (c) 2017 Victor Fisac
+*   Copyright (c) 2017-2020 Victor Fisac
 *
 *   This software is provided "as-is", without any express or implied warranty. In no event
 *   will the authors be held liable for any damages arising from the use of this software.
